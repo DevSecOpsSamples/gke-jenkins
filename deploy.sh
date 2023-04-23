2b00a0645393c208732162327a784a18da0e9570
engel80 <prettyhip@gmail.com>
2023-04-24 00:39:51 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "PROJECT_ID: ${PROJECT_ID}"
 
-docker build -t jenkins-master . --platform linux/amd64
-docker tag jenkins-master:latest gcr.io/${PROJECT_ID}/jenkins-master:latest
-docker push gcr.io/${PROJECT_ID}/jenkins-master:latest
+docker build -t jenkins-master:v1 . --platform linux/amd64
+docker tag jenkins-master:v1 gcr.io/${PROJECT_ID}/jenkins-master:v1
+docker push gcr.io/${PROJECT_ID}/jenkins-master:v1
 
 kubectl scale deployment jenkins-master --replicas=0
 kubectl rollout status deployment jenkins-master
